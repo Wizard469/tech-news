676da3cd5746b8cd82b81f385e571509e03b9e17
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2023-03-14 13:09:16 -0300

    Req 1 - Creating the function 'fetch'

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,19 @@
+import requests
+from time import sleep
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    headers = {"user-agent": "Fake user-agent"}
+    sleep(1)
+    try:
+        response = requests.get(url, headers=headers, timeout=3)
+        if response.status_code != 200:
+            return None
+
+        return response.text
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
