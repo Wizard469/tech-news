0239a02513a778af19387d6a4f800daa1bde014c
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2023-03-20 09:25:30 -0300

    Req 10 - Creating the function 'top_5_categories'

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,9 @@
+from tech_news.database import find_news
+from collections import Counter
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    categories = sorted([news["category"] for news in find_news()])
+
+    return [key[0] for key in Counter(categories).most_common(5)]
